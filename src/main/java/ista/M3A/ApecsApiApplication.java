df6acf37c74fff7bb0ca523bda3de085fead6bbb
Josue Abad <josue.abad.est@tecazuay.edu.ec>
2026-01-16 12:15:10 -0500

    Initial commit

---
+++ src/main/java/ista/M3A/ApecsApiApplication.java
@@ -0,0 +1,13 @@
+package ista.M3A;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApecsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApecsApiApplication.class, args);
+	}
+
+}
